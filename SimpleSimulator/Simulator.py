3671825c0c4fd201576e23aeadf6667408f38e28
Soumil Jain <soumil24639@iiitd.ac.in>
2025-03-22 21:56:42 +0530

    basic code to understand how to start

---
+++ SimpleSimulator/Simulator.py
@@ -0,0 +1,23 @@
+def decode_instruction(instr):
+    opcode = instr[7:]
+
+    if opcode == "0110011": # checks r - type
+        for                 # for loop to store values of registrsss bla bla..
+    
+    if opcode == "0000011": # checks i - type lw
+        for                 # for loop to store values of registrsss bla bla..
+    
+    if opcode == "0010011": # checks i - type addi
+        for                 # for loop to store values of registrsss bla bla..
+    
+    if opcode == "1100111": # checks i - type jallr
+        for                 # for loop to store values of registrsss bla bla..
+
+    if opcode == "1100011": # checks b - type
+        for                 # for loop to store values of registrsss bla bla..
+    
+    if opcode == "1101111": # checks j - type
+        for                 # for loop to store values of registrsss bla bla..
+    
+    
+       
