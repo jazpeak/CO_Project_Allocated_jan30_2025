57d08293f559ebb26656932c2392dcca76bf497b
Soumil Jain <soumil24639@iiitd.ac.in>
2025-03-24 18:20:26 +0530

    mechanism to get lines from a file

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -1,8 +1,9 @@
 def decode_instruction(instr):
+
     opcode = instr[7:]
 
     if opcode == "0110011": # checks r - type
-        for                 # for loop to store values of registrsss bla bla..
+                         # for loop to store values of registrsss bla bla..
     
     if opcode == "0000011": # checks i - type lw
         for                 # for loop to store values of registrsss bla bla..
@@ -10,7 +11,7 @@ def decode_instruction(instr):
     if opcode == "0010011": # checks i - type addi
         for                 # for loop to store values of registrsss bla bla..
     
-    if opcode == "1100111": # checks i - type jallr
+    if opcode == "1100111": # checks i - type jallrdwwd
         for                 # for loop to store values of registrsss bla bla..
 
     if opcode == "1100011": # checks b - type
@@ -19,5 +20,13 @@ def decode_instruction(instr):
     if opcode == "1101111": # checks j - type
         for                 # for loop to store values of registrsss bla bla..
     
+
+
+#The input file will be in binary format. we extract line by line from function below and put it in decode_instruction function
+def run(file_name):
+    fil = open(file_name, "r")
+    fl = fil.readlines()
+    for line in fl:
+        decode_instruction(line.strip())
     
-       
+    fil.close()
