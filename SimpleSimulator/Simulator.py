724ee9063b215cce3feb986120ffc3ffd6d8bd19
Soumil Jain <soumil24639@iiitd.ac.in>
2025-03-24 18:47:50 +0530

    added code to add and subtract fro r type

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -10,8 +10,14 @@ def decode_instruction(instr):
         rs1 = int(instr[12:17], 2)  # sregister 1
         funct3 = instr[17:20]  # funct3
         rd = int(instr[20:25], 2)  # storage register
-        opcode = "0110011"  
-        
+        opcode = "0110011"
+
+        if funct3 == "000":  # checks for add or subtract as funct3 is same for them both
+            if funct7 == "0000000":  # addding
+                registers[rd] = registers[rs1] + registers[rs2]
+            elif funct7 == "0100000":  # subtraacting
+                registers[rd] = registers[rs1] - registers[rs2]
+
     
     if opcode == "0000011": # checks i - type lw
         for                 # for loop to store values of registrsss bla bla..
@@ -30,7 +36,7 @@ def decode_instruction(instr):
     
 
 
-#The input file will be in binary format. we extract line by line from function below and put it in decode_instruction function
+#The input file will be in binary format. we will take line by line from function below and put it in decode_instruction function
 def run(file_name):
     fil = open(file_name, "r")
     fl = fil.readlines()
