653b6f0c2070c7ba61c608aad0e72cac1a320107
aryankhare2110 <aryan24124@iiitd.ac.in>
2025-03-29 18:27:30 +0530

    automated testing done

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -1,52 +1,50 @@
+import sys
 import re
 from re import split
 
-
 testCaseFiles = {"SimpleGen": ["Ex_test_0.txt", "Ex_test_1.txt", "Ex_test_2.txt", "Ex_test_3.txt", "Ex_test_4.txt", "Ex_test_5.txt", "Ex_test_6.txt", "Ex_test_7.txt", "Ex_test_8.txt", "Ex_test_9.txt", "Ex_test_10.txt"], "errorGen": ["Ex_test_3.txt"]}
 
-opCodes = { "R"   : "0110011",
-            "lw"  : "0000011",
+opCodes = { "R" : "0110011",
+            "lw" : "0000011",
             "addi": "0010011",
             "jalr": "1100111",
-            "sw"  : "0100011",
-            "B"   : "1100011",
+            "sw" : "0100011",
+            "B" : "1100011",
             "jal" : "1101111"}
 
-
 registers = {"zero" : "00000",
-             "ra"   : "00001",    
-             "sp"   : "00010",
-             "gp"   : "00011",
-             "tp"   : "00100",
-             "t0"   : "00101",
-             "t1"   : "00110",
-             "t2"   : "00111",
-             "s0"   : "01000",
-             "fp"   : "01000",
-             "s1"   : "01001",
-             "a0"   : "01010",
-             "a1"   : "01011",
-             "a2"   : "01100",
-             "a3"   : "01101",
-             "a4"   : "01110",
-             "a5"   : "01111",
-             "a6"   : "10000",
-             "a7"   : "10001",
-             "s2"   : "10010",
-             "s3"   : "10011",
-             "s4"   : "10100",
-             "s5"   : "10101",
-             "s6"   : "10110",
-             "s7"   : "10111",
-             "s8"   : "11000",
-             "s9"   : "11001",
-             "s10"  : "11010",
-             "s11"  : "11011",
-             "t3"   : "11100",
-             "t4"   : "11101",
-             "t5"   : "11110",
-             "t6"   : "11111"}
-
+             "ra" : "00001", 
+             "sp" : "00010",
+             "gp" : "00011",
+             "tp" : "00100",
+             "t0" : "00101",
+             "t1" : "00110",
+             "t2" : "00111",
+             "s0" : "01000",
+             "fp" : "01000",
+             "s1" : "01001",
+             "a0" : "01010",
+             "a1" : "01011",
+             "a2" : "01100",
+             "a3" : "01101",
+             "a4" : "01110",
+             "a5" : "01111",
+             "a6" : "10000",
+             "a7" : "10001",
+             "s2" : "10010",
+             "s3" : "10011",
+             "s4" : "10100",
+             "s5" : "10101",
+             "s6" : "10110",
+             "s7" : "10111",
+             "s8" : "11000",
+             "s9" : "11001",
+             "s10" : "11010",
+             "s11" : "11011",
+             "t3" : "11100",
+             "t4" : "11101",
+             "t5" : "11110",
+             "t6" : "11111"}
 
 funct3_R = {"add":"000",
             "sub":"000",
@@ -55,7 +53,6 @@ funct3_R = {"add":"000",
             "srl":"101",
             "slt":"010"}
 
-
 funct7_R = {"add":"0000000",
             "sub":"0100000",
             "and":"0000000",
@@ -63,24 +60,19 @@ funct7_R = {"add":"0000000",
             "srl":"0000000",
             "slt":"0000000"}
 
-
 funct3_B = {"beq":"000",
             "bne":"001",
             "blt":"100",} 
 
-
 funct3_I = {"addi":"000",
-            "lw"  :"010",
+            "lw" : "010",
             "jalr":"000"}
 
-
 funct3_S = {"sw":"010"}
-
 
 labels = {}
 instructions = []
 pc = 0
-
 
 def checkType(ins):
     if ins[0] in funct3_R:
@@ -93,11 +85,9 @@ def checkType(ins):
         return btype(ins)
     elif ins[0] == "jal":
         return jtype(ins)
-    
     else:
         raise SyntaxError("Wrong instruction type at line " + str(pc//4 + 1))
     
-
 def decToBinary(n, x):
     d=0
     if n[0]=='-':
@@ -129,7 +119,6 @@ def decToBinary(n, x):
         S = y + S
     return S
 
-
 def itype(ins):
     r = ""
     if len(ins)==3:
@@ -149,9 +138,7 @@ def itype(ins):
         if (not imm.lstrip('-').isdigit()) or (int(imm)>2047 or int(imm)<-2048):
             raise SyntaxError("Immediate value out of range at line " + str(pc//4 + 1))
     r = decToBinary(imm,12)+registers[rs]+funct3_I[ins[0]]+registers[ins[1]]+opCodes[ins[0]]
-
     return r
-
 
 def stype(ins):
     ins[2] = ins[2].rstrip(')')
@@ -159,14 +146,12 @@ def stype(ins):
     imm = x[0]
     if ins[1] not in registers or x[1] not in registers:
         raise SyntaxError("Wrong register name at line " + str(pc//4 + 1))
-    
     else:
         if (not imm.lstrip('-').isdigit()) or (int(imm) > 2047 or int(imm) < -2048):
             raise SyntaxError("Immediate value out of range at line " + str(pc//4 + 1))
         final_imm = decToBinary(imm, 12)
         s = final_imm[:7] + registers[ins[1]] + registers[x[1]] + funct3_S[ins[0]] + final_imm[7:] + opCodes['sw']
     return s
-
 
 def rtype(ins):
     if ins[3] not in registers or ins[2] not in registers or ins[1] not in registers:
@@ -192,7 +177,6 @@ def btype(ins):
     r = x[0] + x[2:8] + registers[ins[2]] + registers[ins[1]] + funct3_B[ins[0]] + x[-4:] + x[1] + opCodes['B']
     return r
 
-
 def jtype(ins):
     y = ins[2].strip()
     if y in labels:
@@ -207,7 +191,6 @@ def jtype(ins):
     r = x[0] + x[-10:] + x[-11] + x[1:9] + registers[ins[1]] + opCodes['jal']
     return r
 
-
 def checkLabel(s, label=0):
     global pc
     if label != 0:
@@ -215,7 +198,6 @@ def checkLabel(s, label=0):
     if s and not (len(s) == 1 and s[0] == ''):
         instructions.append(s)
         pc += 4
-
 
 def fileRead (file_name):
     with open(file_name, 'r') as file:
@@ -234,7 +216,6 @@ def fileRead (file_name):
                 s=re.split(pattern=r"[,. ]", string=s[0])
             checkLabel(s,label)
 
-
 def fileOutput (Output):
     if instructions[-1] != ["beq", "zero", "zero", "0"]:
         raise SyntaxError("Missing virtual halt instruction at the end")
@@ -245,78 +226,20 @@ def fileOutput (Output):
             file.write(checkType(ins) + '\n')
             pc+=4
 
+if len(sys.argv) < 3:
+    print("Usage: python script.py <input_file_path> <output_file_path>")
+    sys.exit(1)
 
-file_name = "D:/CO_Project_Allocated_jan30_2025/CO_Project_Allocated_jan30_2025/SimpleAssembler/input.txt"
-output = "D:/CO_Project_Allocated_jan30_2025/CO_Project_Allocated_jan30_2025/SimpleAssembler/output.txt"
+input_file_path = sys.argv[1]
+output_file_path = sys.argv[2]
 
-fileRead(file_name)
-fileOutput(output)
-
-
-#GRP-34-Jazl-Aryan-Soumil-Arul
-'''
-
-                                            %%% ##   ##
-                                         %%%%% ###%%###
-                                        %%%%% ### %%% #
-                                      %%%%%% ### %%% ###
-                                       %%%% ## %% #######
-                                      %%%%% # %% #O#####
-                                    %%%%%% # % #########
-                                   %%%%% ##### #########
-                         ###        %% ####### #########
-                %%% ############    ########### ########
-             %%%% ############################### #######
-           %%%%% ################################## ######
-         %%%%%% #################################### #C###
-        %%%%%% #####################################  ###
-        %%%%% #######################################
-       %%%%%% ########################################
-    % %%%%%%% ########################################
-     %%%%%%%%% #######################################
-    %%%%%%%%%% ########################################
- %%% %%%%%%%%   ###### ################################
-   %%%%%%%%      ###### #################### ##########
-% %%%%%%%%        ####### ########### ###### ##########
- %%%%%%%%%         #######  ########### ###### ########
-%%%%%%%%%%          ##### ###  ######### ####### ######
- %%%%%%%%%%          #### ##               ####### ####
- %%%%%%%%%%%           ## #                  ##### ###
-  %%  %% % %%         # ##                      ## ###
-    %   %    %        # ###                      # ###
-                       # ###                     ## ###
-                       # ###                     ## ###
-                       # ####                   #### ##
-                      ### ###                  ##### ###
-                     ####  ###                 ####   ##
-                    #####   ###                 ##    ##
-                   #####    ####                      ###
-                    ##        ###                     ###
-                               ####                     ##
-                                ####                    ###
-                                                        ####
-                                                         ##
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
+try:
+    fileRead(input_file_path)
+    fileOutput(output_file_path)
+    print(f"Assembly code processed successfully. Output written to {output_file_path}")
+except FileNotFoundError:
+    print(f"Error: Input file '{input_file_path}' not found.")
+except SyntaxError as e:
+    print(f"Syntax Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
